1996b527dc223fbcc93f04da06c689736a29a79d
drKreso <kresimir.bojcic@gmail.com>
2018-08-08 23:14:01 +0200

    skb

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,7 +1,8 @@
 rails_env = new_resource.environment["RAILS_ENV"]
+secret_key_base = new_resource.environment["SECRET_KEY_BASE"]
 Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
 execute "rake assets:precompile" do
   cwd release_path
   command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => rails_env
+  environment "RAILS_ENV" => rails_env, "SECRET_KEY_BASE" => secret_key_base
 end
