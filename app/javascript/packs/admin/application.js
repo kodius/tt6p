15c66f73ae02f4c03bf4ed9512dd8d2272000447
matija <matija@outlook.com>
2018-08-10 02:23:38 +0200

    wrong admin api path

--- app/javascript/packs/admin/application.js
+++ app/javascript/packs/admin/application.js
@@ -15,7 +15,7 @@ Vue.component('nav-top', NavTop);
 import SubmitTag from './components/shared/_submit_tag';
 Vue.component('submit-tag', SubmitTag);
 
-axios.defaults.baseURL = process.env.API_BASE_URL
+axios.defaults.baseURL = process.env.API_BASE_URL + 'admin/'
 axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector("meta[name=csrf-token]").content
 
 // $.ajaxSetup({
