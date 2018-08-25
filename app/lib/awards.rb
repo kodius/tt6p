aae142f625c961055d2e77f390a9213668de2ec3
drKreso <kresimir.bojcic@gmail.com>
2018-08-25 08:46:30 +0200

    Lowest BF award

--- app/lib/awards.rb
+++ app/lib/awards.rb
@@ -4,6 +4,8 @@ class Awards
     # LONEGEST STREAK
     # MOST FAT LOST
     # MOST BF% LOST
+    # LOWEST BF%
+
     def self.get_all
         result = {}
         Plan.all.each do |plan|
@@ -15,11 +17,13 @@ class Awards
         longest_streak = plans.sort_by { |p| p.days_streak }.last
         most_fat_lost = plans.sort_by { |p| (p.body_fat - p.last_body_fat) }.last
         most_bf_lost_percent = plans.sort_by { |p| p.days_streak }.last
+        lowest_bf_percent = plans.sort_by { |p| p.last_body_fat }.last
         result[fat_fuck.id] = result[fat_fuck.id]  << "FAT FUCK"
         result[strongman.id] = result[strongman.id]  << "STRONGMAN"
         result[longest_streak.id] = result[longest_streak.id]  << "LONEGEST STREAK"
         result[most_fat_lost.id] = result[most_fat_lost.id]  << "MOST FAT LOST"
         result[most_bf_lost_percent.id] = result[most_bf_lost_percent.id]  << "MOST BF% LOST"
+        result[lowest_bf_percent.id] = result[lowest_bf_percent.id]  << "LOWEST BF%"
         return result
     end
 end
