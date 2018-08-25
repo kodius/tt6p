d3a520e38133ee1c8542f2d67b32b3d0fc207c85
drKreso <kresimir.bojcic@gmail.com>
2018-08-25 08:44:50 +0200

    Bugic :)

--- app/lib/awards.rb
+++ app/lib/awards.rb
@@ -7,14 +7,14 @@ class Awards
     def self.get_all
         result = {}
         Plan.all.each do |plan|
-            result[plan.id] = []
+          result[plan.id] = []
         end
         plans = Plan.all
-        fat_fuck = plans.sort_by { |p| p.last_body_fat }.first
-        strongman = plans.sort_by { |p| p.last_lean_body_mass }.first
-        longest_streak = plans.sort_by { |p| p.days_streak }.first
-        most_fat_lost = plans.sort_by { |p| (p.body_fat - p.last_body_fat) }.first
-        most_bf_lost_percent = plans.sort_by { |p| p.days_streak }.first
+        fat_fuck = plans.sort_by { |p| p.last_body_fat }.last
+        strongman = plans.sort_by { |p| p.last_lean_body_mass }.last
+        longest_streak = plans.sort_by { |p| p.days_streak }.last
+        most_fat_lost = plans.sort_by { |p| (p.body_fat - p.last_body_fat) }.last
+        most_bf_lost_percent = plans.sort_by { |p| p.days_streak }.last
         result[fat_fuck.id] = result[fat_fuck.id]  << "FAT FUCK"
         result[strongman.id] = result[strongman.id]  << "STRONGMAN"
         result[longest_streak.id] = result[longest_streak.id]  << "LONEGEST STREAK"
