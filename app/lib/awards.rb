56bd1f4b7f0e806b3ae870b1194966ce3b782729
drKreso <kresimir.bojcic@gmail.com>
2018-08-25 09:08:16 +0200

    Fix typo

--- app/lib/awards.rb
+++ app/lib/awards.rb
@@ -20,7 +20,7 @@ class Awards
         lowest_bf_percent = plans.sort_by { |p| p.last_body_fat }.first
         result[fat_fuck.id] = result[fat_fuck.id]  << "FAT FUCK"
         result[strongman.id] = result[strongman.id]  << "STRONGMAN"
-        result[longest_streak.id] = result[longest_streak.id]  << "LONEGEST STREAK"
+        result[longest_streak.id] = result[longest_streak.id]  << "LONGEST STREAK"
         result[most_fat_lost.id] = result[most_fat_lost.id]  << "MOST FAT LOST"
         result[most_bf_lost_percent.id] = result[most_bf_lost_percent.id]  << "MOST BF% LOST"
         result[lowest_bf_percent.id] = result[lowest_bf_percent.id]  << "LOWEST BF%"
