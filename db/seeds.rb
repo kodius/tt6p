ec43704ec9d9c4da01ba59d61bd424e0bab64f67
zivaskovic <zvonimir.ivaskovic@gmail.com>
2018-08-29 20:13:48 +0200

    Fixing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,8 @@
 User.delete_all
-Musician.delete_all
 Plan.delete_all
 
 User.create!(email: "admin@domain.com", password: "password", password_confirmation: "password", id: 1)
-Musician.create!(name: "John Lennon", band: "beatles")
-Musician.create!(name: "Paul McCartney", band: "beatles")
-Musician.create!(name: "Georges Harrison", band: "beatles")
-Musician.create!(name: "Ringo Starr", band: "beatles")
-Musician.create!(name: "Mick Jagger", band: "rolling_stones")
-Musician.create!(name: "Keith Richards", band: "rolling_stones")
-Musician.create!(name: "Mick Taylor", band: "rolling_stones")
-Musician.create!(name: "Bill Wyman", band: "rolling_stones")
-Musician.create!(name: "Charlie Watts", band: "rolling_stones")
-Musician.create!(name: "Angus Young", band: "acdc")
-Musician.create!(name: "Malcom Young", band: "acdc")
-Musician.create!(name: "Bon Scott", band: "acdc")
-Musician.create!(name: "Phil Rudd", band: "acdc")
+User.create!(email: "user@kodius.io", password: "password", password_confirmation: "password", id: 2)
 
 Plan.create(user_id: User.last.id, weight: 100, :body_fat => 25, target_body_fat: 13, height: 183, age: 41, gender: 'man', activity_level: 'moderatly_active' )
 
