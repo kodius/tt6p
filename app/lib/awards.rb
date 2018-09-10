fd8f1153815e3bcecde9322b54ced469fdea09ca
drKreso <kresimir.bojcic@gmail.com>
2018-09-10 08:46:19 +0200

    Awards to the one participating

--- app/lib/awards.rb
+++ app/lib/awards.rb
@@ -6,21 +6,22 @@ class Awards
     # LOWEST BF%
 
     def self.get_all
-        result = {}
-        Plan.all.each do |plan|
-          result[plan.id] = []
-        end
-        plans = Plan.all
-        strongman = plans.sort_by { |p| p.last_lean_body_mass }.last
-        longest_streak = plans.sort_by { |p| p.days_streak }.last
-        most_fat_lost = plans.sort_by { |p| p.fat_lost }.last
-        most_bf_lost_percent = plans.sort_by { |p| (p.body_fat - p.last_body_fat) }.last
-        lowest_bf_percent = plans.sort_by { |p| p.last_body_fat }.first
-        result[strongman.id] = result[strongman.id]  << "STRONGMAN"
-        result[longest_streak.id] = result[longest_streak.id]  << "LONGEST STREAK"
-        result[most_fat_lost.id] = result[most_fat_lost.id]  << "MOST FAT LOST"
-        result[most_bf_lost_percent.id] = result[most_bf_lost_percent.id]  << "MOST BF% LOST"
-        result[lowest_bf_percent.id] = result[lowest_bf_percent.id]  << "LOWEST BF%"
-        return result
+      result = {}
+      active_user_ids = Measurement.select("user_id").group(:user_id).having("MAX(log_date) >= ?", DateTime.now - 1.week).pluck(:user_id)
+      plans = Plan.where("user_id in (?)", active_user_ids)
+      plans.each do |plan|
+        result[plan.id] = []
+      end
+      strongman = plans.sort_by { |p| p.last_lean_body_mass }.last
+      longest_streak = plans.sort_by { |p| p.days_streak }.last
+      most_fat_lost = plans.sort_by { |p| p.fat_lost }.last
+      most_bf_lost_percent = plans.sort_by { |p| (p.body_fat - p.last_body_fat) }.last
+      lowest_bf_percent = plans.sort_by { |p| p.last_body_fat }.first
+      result[strongman.id] = result[strongman.id]  << "STRONGMAN"
+      result[longest_streak.id] = result[longest_streak.id]  << "LONGEST STREAK"
+      result[most_fat_lost.id] = result[most_fat_lost.id]  << "MOST FAT LOST"
+      result[most_bf_lost_percent.id] = result[most_bf_lost_percent.id]  << "MOST BF% LOST"
+      result[lowest_bf_percent.id] = result[lowest_bf_percent.id]  << "LOWEST BF%"
+      return result
     end
 end
