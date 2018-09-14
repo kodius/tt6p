0a231c9127811ffdf9d8a3fa485371c880a0c42c
istuk <ilija.istuk@outlook.com>
2018-09-14 10:28:20 +0200

    Ups 😅

--- app/controllers/api/measurements_controller.rb
+++ app/controllers/api/measurements_controller.rb
@@ -10,8 +10,6 @@ class Api::MeasurementsController < Api::ApiController
     plan = Plan.find(params[:id])
     @measurements = Measurement.where("user_id = ?", plan.user_id).order('log_date desc')
     @count = @measurements.count()
-    pp 'kurac'
-    pp params
     @measurements = @measurements.order('log_date desc').offset((params[:page].to_i - 1) * 20).limit(20)
   end
 
