174bedf3be541afb69bd759ece6ba008a13c2d78
drKreso <kresimir.bojcic@gmail.com>
2018-10-18 19:58:20 +0200

    Fixing bug with logging in past from "log" form

--- app/controllers/api/measurements_controller.rb
+++ app/controllers/api/measurements_controller.rb
@@ -35,18 +35,27 @@ class Api::MeasurementsController < Api::ApiController
   end
 
   def create
-    @measurement = Measurement.find_by_id(measurement_params['id'])
-    if @measurement.blank?
-      @measurement = Measurement.new(measurement_params)
-      log_date = (@measurement.log_date + 2.hours).to_date
-      @measurement.log_date = log_date
-      @measurement.user = current_user
-      #make sure to remove doubles - only one log date per user measurement is allowed
-      Measurement.where("user_id = ? and log_date = ?", @measurement.user_id, log_date).delete_all
+    measurement = Measurement.find_by_id(measurement_params['id']) 
+    params[:measurement].delete :id
+    new_measurement = Measurement.new(measurement_params)
+    log_date = (new_measurement.log_date + 2.hours).to_date
+
+    #this will be used if no log is found
+    new_measurement.log_date = log_date
+    new_measurement.user = current_user
+
+    if measurement.blank?
+      measurement = Measurement.where("log_date = ? and user_id = ?", log_date, current_user.id).first || new_measurement
     else
-      @measurement.update(measurement_params)
+      #promjenili smo dan ili treba naci taj log ili napraviti novi
+      if measurement.log_date != log_date
+        measurement = Measurement.where("log_date = ? and user_id = ?", log_date, current_user.id).first || new_measurement
+      end
     end
-    @measurement.save!
+
+    measurement.update(measurement_params)
+    measurement.log_date = log_date
+    measurement.save!
   end
 
   def destroy
@@ -69,10 +78,12 @@ class Api::MeasurementsController < Api::ApiController
     else
       @measurement = Measurement.new
       if last_measurement
+        @measurement.user = current_user
         @measurement.weight = last_measurement.weight
         @measurement.body_fat = last_measurement.body_fat
       end
       @measurement.log_date = DateTime.now.to_date
+      @measurement.save!
     end
   end
 
