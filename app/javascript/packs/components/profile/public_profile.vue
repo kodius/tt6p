a975a33532b89df5e89177be780db179e0b41bf1
drKreso <kresimir.bojcic@gmail.com>
2018-11-04 09:15:18 +0100

    Reuse code

--- app/javascript/packs/components/profile/public_profile.vue
+++ app/javascript/packs/components/profile/public_profile.vue
@@ -1,78 +1,17 @@
 <template>
   <layout>
-
     <div class="container">
-
       <div class="notification">
-        <div v-if="loaded">
-          <div class="tile is-ancestor">
-              <div class="tile is-parent">
-                  <div class="tile is-child box is-12 notification">
-                      <p class="title">{{ plan.email }} </p>
-                      <p class="subtitle">{{ plan.awards }}</p>
-                  </div>
-              </div>
+        <div v-if="initialLoad">
+          <awards :plan="plan" />
+          <div v-if="bodyMass.loaded">
+            <measurement-chart :height="300" :labels="bodyMass.labels" :datasets1="bodyMass.totalBodyMass" :datasets="bodyMass.leanBodyMass"
+              label1="Weight(kg)" label="Lean Body Mass(kg)" stepSize=10></measurement-chart>
           </div>
-          <div v-if="loadedBodyMass">
-            <measurement-chart :height="300" :labels="bodyMasssLabels" :datasets1="bodyMassDatasets" :datasets="leanBodyMassDatasets" label1="Weight(kg)" label="Lean Body Mass(kg)" stepSize=10></measurement-chart>
-          </div>
-          <div class="tile is-ancestor">
-            <div class="tile is-4 is-vertical is-parent">
-              <article class="tile is-child notification is-warning box">
-                <p class="title">Body Stats</p>
-                <strong>Weight:</strong> {{ plan.lastWeight }} kg
-                <br/>
-                <strong>Body Fat:</strong> {{ plan.lastBodyFat }}%
-                <br/>
-                <strong>Gender: </strong>{{ plan.gender | humanize }}
-                <br/>
-                <strong>Activity Level:</strong> {{ plan.activityLevel | humanize }}
-                <br/>
-                <strong>LBM Difference:</strong> {{ plan.lbmDiff }} kg
-              </article>
-              <article class="tile is-child notification is-info box">
-                <p class="title">Target Stats</p>
-                <strong>Target Body Fat: </strong>{{ plan.targetBodyFat }}%
-                <br/>
-                <strong>Target Weight: </strong>{{ plan.targetWeight }} kg
-              </article>
-            </div> 
-            <div class="tile is-parent">
-              <div class="tile is-child box">
-                <p class="title">Lean Mass & Calories</p>
-                <div class="tile">
-                  <div class="tile is-child box is-6">
-                    <p class="title">LBM</p>
-                    <p class="subtitle">{{ plan.lastLbm }} kg</p>
-                  </div>
-                  <div class="tile is-child box is-6">
-                    <p class="title">TDEE</p>
-                    <p class="subtitle">{{ plan.tdee }} kcal</p>
-                  </div>
-                </div>
-                <div class="tile">
-                  <div class="tile is-child box is-6">
-                    <p class="title">Days Till Sixpack</p>
-                    <p class="subtitle">{{ plan.daysTillSixpack }} days</p>
-                  </div>
-                  <div class="tile is-child box is-6  notification is-success">
-                    <p class="title">Sixpack Day</p>
-                    <p class="subtitle">{{ plan.dayOfSixpack }}</p>
-                  </div>
-                </div>
-                <div class="tile">
-                  <div class="tile is-child box is-6 notification is-danger">
-                    <p class="title">Fat Lost</p>
-                    <p class="subtitle">{{ plan.fatLost }} kg</p>
-                  </div>
-                  <div class="tile is-child box is-6  notification is-success">
-                    <p class="title">Streak</p>
-                    <p class="subtitle">{{plan.daysStreak}} {{plan.daysStreak | pluralize('day')}}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <stats :plan="plan" />
+          <p>
+            <router-link :to="{ name: 'log_path' }" class="button is-large is-primary" role="button">Log data</router-link>
+          </p>
         </div>
         <div v-else>
           Loading...
@@ -80,152 +19,66 @@
         <br />
       </div>
     </div>
-    <div v-if="loaded">
-      <table class="table is-bordered is-striped is-fullwidth">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Weight</th>
-            <th>Calories</th>
-            <th>BF%</th>
-            <th>LBM</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-        <tr v-for="measurement in measurements" 
-            v-bind:key="measurement.id">
-          <td>{{ measurement.logDate }} </td>
-          <td>{{ measurement.weight }} kg</td>
-          <td>
-            <span v-if="measurement.calories">
-              {{ measurement.calories }} kcal
-            </span>
-            <span v-else>
-              -
-            </span>
-          </td>
-          <td>
-            <span v-if="measurement.bodyFat">
-              {{ measurement.bodyFat }}%
-            </span>
-            <span v-else>
-              -
-            </span>
-          </td>
-          <td>
-            <span v-if="measurement.lbm">
-              {{ measurement.lbm }} kg
-            </span>
-            <span v-else>
-              -
-            </span>
-          </td>
-          <td>
-            <span v-if="measurement.success" class="icon has-text-success">
-              <i class="fas fa-check-square"></i>
-            </span>
-            <span v-if="measurement.success == false" class="icon has-text-danger">
-              <i class="fas fa-minus-square"></i>
-            </span>
-          </td>
-        </tr>
-        </tbody>
-      </table>
-      <div v-if="total > 20"> 
-        <b-pagination
-          :total="total"
-          :current.sync="page"
-          :per-page="20"
-          order="is-centered"
-          @change="loadMeasurements">
-        </b-pagination>
-      </div>
+    <div v-if="calories.loaded">
+      <measurement-chart :height="300" :labels="calories.labels" :datasets="calories.totalCalories" :datasets1="calories.allowedCalories"
+        label="Calories(kcal)" stepSize=500 label1="TDEE calories(kcal)"></measurement-chart>
     </div>
+    <section class="section" />
+    <kodius-footer />
   </layout>
 </template>
 
 <script>
-import axios from 'axios';
-import Layout from '../shared/layout';
-import Vue from 'vue/dist/vue.esm';
-import humanizeString from 'humanize-string';
-import MeasurementChart from '../charts/measurement_chart';
+  import axios from 'axios';
+  import Buefy from 'buefy'
+  import Layout from '../shared/layout';
+  import MeasurementChart from '../charts/measurement_chart';
+  import Vue from 'vue/dist/vue.esm';
+  import Awards from '../dashboard/awards'
+  import Stats from '../dashboard/stats'
+  import Footer from '../shared/footer'
+  import MeasurementsTable from '../dashboard/measurements_table'
 
-Vue.component('measurement-chart', MeasurementChart);
+  Vue.component('measurement-chart', MeasurementChart);
+  Vue.component('measurements-table', MeasurementsTable);
+  Vue.component('awards', Awards);
+  Vue.component('stats', Stats);
+  Vue.component('kodius-footer', Footer);
+  Vue.use(Buefy)
 
-export default {
-  components: {
-    Layout,
-    MeasurementChart
-  },
-  data () {
-    return {
-      loaded: false,
-      plan: null,
-      measurements: [],
-      page: 1,
-      total: 0,
-      currentDimension: 'week',
-      measurements: [],
-      bodyMassDatasets: [],
-      leanBodyMassDatasets: [],
-      loadedBodyMass: false,
-      bodyMasssLabels: []
-    }
-  },
-  filters: {
-    humanize(text) {
-      return humanizeString(text);
-    }
-  },
-  mounted () {
-    this.loadedBodyMass = false
-    self = this
-    axios
-      .get('public-profile/' + this.$route.params.id)
-      .then(response => {
-        self.plan = response.data.plan
-        axios
-          .get(`public-measurements/${this.$route.params.id}/1`)
-          .then(response => {
-              self.measurements = response.data.measurements;
-              self.total = response.data.count;
-              self.loaded = true
-        })
-      })
-    this.loadBodyMassChartData()
-  },
-  methods: {
-    loadMeasurements() {
-      var that = this;
-      setTimeout(() => axios
-        .get(`public-measurements/${this.$route.params.id}/${this.page}`)
-        .then(response => {
-            that.measurements = response.data.measurements
-            that.total = response.data.count
-      }), 50);
+  export default {
+    components: {
+      Layout,
+      MeasurementChart
     },
-    loadBodyMassChartData() {
-      var that = this;
-      axios
-      .post('chart-data', {
-        dimension: that.currentDimension,
-        id: that.$route.params.id,
-        average_on: 'weight'
+    data() {
+      return {
+        currentDimension: 'week'
+      }
+    },
+    computed: {
+      initialLoad() {
+        return this.$store.state.currentUser.initialLoad
+      },
+      plan() {
+        return this.$store.state.currentUser.plan
+      },
+      bodyMass() {
+        return this.$store.state.currentUser.bodyMass
+      },
+      calories() {
+        return this.$store.state.currentUser.calories
+      }
+    },
+    mounted() {
+      this.$store.dispatch('currentUser/loadPlan')
+      this.$store.dispatch('currentUser/loadBodyMass', {
+        dimension: this.currentDimension
       })
-      .then(response => {
-        for (var idx in response.data[0]) {
-          that.bodyMasssLabels.push(that.currentDimension + ' ' + idx) 
-          that.bodyMassDatasets.push(response.data[0][idx]) 
-        }
-        for (var idx in response.data[1]) {
-          that.leanBodyMassDatasets.push(response.data[1][idx]) 
-        }
-        that.loadedBodyMass = true
+      this.$store.dispatch('currentUser/loadCalories', {
+        dimension: this.currentDimension
       })
     },
+
   }
-}
-
 </script>
